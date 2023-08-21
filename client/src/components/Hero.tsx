d5c93eb254d88b4dbcaa70c0dfb16289878ec585
kokicha23 <konstantin.marinov30@gmail.com>
2023-08-22 00:45:12 +0300

    added a nav bar and a hero components + styles

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-background"></div>
+            <div className="hero-content">
+                <h1 className="hero-title">Welcome to Our Real Estate Website</h1>
+                <p className="hero-description">Find Your Dream Home Today</p>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import logo from "../logo.png"
+import WebFont from 'webfontloader';
+
+function NavigationBar() {
+    useEffect(() => {
+        WebFont.load({
+            google: {
+                families: ['Lato']
+            }
+        });
+    }, []);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
+            <div className="logo">
+                <img src={logo} alt="Real Estate Logo" />
+            </div>
+            <div className={`menu-toggle ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <ul className="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Properties</a></li>
+                <li><a href="#">Agents</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavigationBar;
